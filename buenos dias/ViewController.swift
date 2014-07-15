90cabef87bde37bdca7b1722af663987d5474524
Caleb Tutty <caleb@prettymint.co.nz>
2014-07-16 00:16:50 +1200

    Lists available Bonjour devices

--- buenos dias/ViewController.swift
+++ buenos dias/ViewController.swift
@@ -8,55 +8,105 @@
 
 import UIKit
 
-class ViewController: UITableViewController, NSNetServiceBrowserDelegate {
-    var services = []
+class ViewController: UITableViewController, NSNetServiceBrowserDelegate, NSNetServiceDelegate {
+    var serviceList = NSMutableArray()
     let serviceBrowser = NSNetServiceBrowser()
     var serviceResolver = NSNetService()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.serviceBrowser.delegate = self
-        
+        self.searchForBonjourServices()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func searchForBonjourServices() {
+        // types:
+        // https://developer.apple.com/library/mac/qa/qa1312/_index.html
+        
+        self.serviceBrowser.searchForServicesOfType("_http._tcp", inDomain: "local.")
+        //self.serviceBrowser.searchForServicesOfType("_music._tcp", inDomain: "local.")
+        //self.serviceBrowser.searchForServicesOfType("_daap._tcp", inDomain: "local.")
+        //self.serviceBrowser.searchForServicesOfType("_printer._tcp", inDomain: "local.")
+        //self.serviceBrowser.searchForServicesOfType("_ipp._tcp", inDomain: "local.")
+        //self.serviceBrowser.searchForServicesOfType("_eppc._tcp", inDomain: "local.")
+        //self.serviceBrowser.searchForServicesOfType("_ssh._tcp", inDomain: "local.")
+        //self.serviceBrowser.searchForServicesOfType("_afpovertcp._tcp", inDomain: "local.")
+        
+        // AirTunes (and AirPlay)
+        //self.serviceBrowser.searchForServicesOfType("_raop._tcp", inDomain: "local.")
+        
+    }
 
     
     // UITableViewDataSourceDelegate
-    
     override func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
-        if(self.services.count == 0) {
+        if(self.serviceList.count == 0) {
             return 1
         } else {
-            return self.services.count
+            return self.serviceList.count
         }
     }
     
     override func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
-        var cell = tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
-        if (cell == nil) {
+        var cell = tableView.dequeueReusableCellWithIdentifier("cell") as? UITableViewCell
+        
+        if !cell {
             cell = UITableViewCell(style: .Default, reuseIdentifier: "cell")
         }
-        if(self.services.count == 0) {
-            cell.textLabel.text = "Searching"
+        
+        
+        if(self.serviceList.count == 0) {
+            cell!.textLabel.text = "Searching"
         } else {
-            cell.textLabel.text = self.services[indexPath.row].name
+            cell!.textLabel.text = self.serviceList[indexPath.row].name
         }
         
         return cell
     }
     
     // UITableViewDelegate
-    
     override func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
         self.serviceResolver.stop()
         
         
-        if(self.services.count != 0) {
-            
+        if(self.serviceList.count != 0) {
+            self.serviceResolver = self.serviceList[indexPath.row] as NSNetService
+            self.serviceResolver.delegate = self
+            self.serviceResolver.resolveWithTimeout(0.0)
+        }
+    }
+    
+    // NSNetServiceDelegate
+    func netServiceDidResolveAddress(sender: NSNetService!) {
+        self.serviceResolver.stop()
+        NSLog("\(sender)")
+    }
+    
+    func netService(sender: NSNetService!, didNotResolve errorDict: [NSObject : AnyObject]!) {
+        self.serviceResolver.stop()
+        NSLog("\(errorDict)")
+    }
+    
+    // NSNetServiceBrowserDelegate
+    func netServiceBrowser(aNetServiceBrowser: NSNetServiceBrowser!, didFindService aNetService: NSNetService!, moreComing: Bool) {
+        
+        self.serviceList.addObject(aNetService)
+        
+        if(!moreComing) {
+            self.tableView.reloadData()
+        }
+    }
+    
+    func netServiceBrowser(aNetServiceBrowser: NSNetServiceBrowser!, didRemoveService aNetService: NSNetService!, moreComing: Bool) {
+        self.serviceList.removeObject(aNetService)
+        
+        if(!moreComing) {
+            self.tableView.reloadData()
         }
     }
 
